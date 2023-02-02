6435e031b8bd9aee1691ff0a00daeec3f92f496c
kch <skso135@naver.com>
2023-02-02 17:33:32 +0900

    230202퇴근

--- RMS.py
+++ RMS.py
@@ -4,7 +4,7 @@
 
 
 
-import platform, psutil, schedule, datetime, pymssql
+import os, platform, psutil, schedule, datetime, pymssql
 
 # 함수 선언
 def message():
@@ -27,13 +27,17 @@ def message():
     #print("<데이터 수집시간:{}>\n<수집주기:{}분>".format(str(current_time),rep_time))
     #print("<수집정보>\n{}".format())
 
-    #OS 종류, 버전, PC명
-    print(psutil)
+    #OS 종류, 버전, PC명, ip, mac
     os_kind = platform.system()
     os_ver = platform.release()
     pc_name = platform.node()
+    ip_info = psutil.net_if_addrs()
+    
+    # ip_mac = 
+    print(ip_info.get('이더넷')[1].address)
 
     #cpu 사용율
+    cpu_used = psutil.cpu_percent()
     #메모리 ttl, 사용량, 사용율
     #네트워크 다운로드양, 업로드양, 초당수신트래픽, 초당발신트래픽
     #디스트 경로, ttl, 사용량, 사용율
@@ -42,13 +46,20 @@ def message():
     con.close() #DB연결해제
 
 
+    
 
-
-
+# dict_items([
+# ('이더넷', [snicaddr(family=<AddressFamily.AF_LINK: -1>, address='B0-25-AA-3B-6C-3F', netmask=None, broadcast=None, ptp=None), snicaddr(family=<AddressFamily.AF_INET: 2>, address='192.168.13.104', netmask='255.255.255.0', broadcast=None, ptp=None), snicaddr(family=<AddressFamily.AF_INET6: 23>, address='fe80::b6a4:b22c:3d1:6c75', netmask=None, broadcast=None, ptp=None)]), 
+# ('Wi-Fi', [snicaddr(family=<AddressFamily.AF_LINK: -1>, address='34-2E-B7-EA-B0-D7', netmask=None, broadcast=None, ptp=None), snicaddr(family=<AddressFamily.AF_INET: 2>, address='169.254.183.32', netmask='255.255.0.0', broadcast=None, ptp=None), snicaddr(family=<AddressFamily.AF_INET6: 23>, address='fe80::e348:629c:2cdf:a503', netmask=None, broadcast=None, ptp=None)]), 
+# ('로컬 영역 연결* 1', [snicaddr(family=<AddressFamily.AF_LINK: -1>, address='34-2E-B7-EA-B0-D8', netmask=None, broadcast=None, ptp=None), snicaddr(family=<AddressFamily.AF_INET: 2>, address='169.254.74.106', netmask='255.255.0.0', broadcast=None, ptp=None), snicaddr(family=<AddressFamily.AF_INET6: 23>, address='fe80::6627:14e3:da95:ccd5', netmask=None, broadcast=None, ptp=None)]), 
+# ('로컬 영역 연결* 10', [snicaddr(family=<AddressFamily.AF_LINK: -1>, address='36-2E-B7-EA-B0-D7', netmask=None, broadcast=None, ptp=None), snicaddr(family=<AddressFamily.AF_INET: 2>, address='169.254.201.254', netmask='255.255.0.0', broadcast=None, ptp=None), snicaddr(family=<AddressFamily.AF_INET6: 23>, address='fe80::71df:bcff:bbc3:f872', netmask=None, broadcast=None, ptp=None)]), 
+# ('Bluetooth 네트워크 연결 3', [snicaddr(family=<AddressFamily.AF_LINK: -1>, address='34-2E-B7-EA-B0-DB', netmask=None, broadcast=None, ptp=None), snicaddr(family=<AddressFamily.AF_INET: 2>, address='169.254.155.156', netmask='255.255.0.0', broadcast=None, ptp=None), snicaddr(family=<AddressFamily.AF_INET6: 23>, address='fe80::fb60:e766:d792:ab3b', netmask=None, broadcast=None, ptp=None)]), 
+# ('Loopback Pseudo-Interface 1', [snicaddr(family=<AddressFamily.AF_INET: 2>, address='127.0.0.1', netmask='255.0.0.0', broadcast=None, ptp=None), snicaddr(family=<AddressFamily.AF_INET6: 23>, address='::1', netmask=None, broadcast=None, ptp=None)])
+# ])
 
 # 실행소스
 rep_time = 10                               # 반복주기
-schedule.every(5).seconds.do(message)       # 반복설정
+schedule.every(3).seconds.do(message)       # 반복설정
 #schedule.every(10).minutes.do(message)     # 반복설정
 
 
