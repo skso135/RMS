5a0c7294809761abd5ee8cb82e68408a4d24f974
kch <skso135@naver.com>
2023-02-06 11:24:48 +0900

    수집주기 수동입력으로 변경

--- RMS.py
+++ RMS.py
@@ -76,7 +76,7 @@ def resource_trace():
         print("경로:{} | 총용량:{} | 사용량:{} | 사용율:{}% | 여유율:{}%".format(disk_path,convert_size(disk_ttl),convert_size(disk_used),disk_userate,disk_availrate))
         #####insert query문#####
         sqlquery += ("insert into rms100 values('{}','{}','{}','{}','{}','{}',{},{},{},{},{},{},{},{},{},'{}',{},{},{},{},'{}');"
-        .format((ip_mac+"_"+ip_info),os_kind,os_ver,pc_name,ip_info,ip_mac,cpu_ㅅused,mem_ttl,mem_used,mem_userate,mem_availrate,down_ttl,up_ttl,down_speed,up_speed,disk_path,disk_ttl,disk_used,disk_userate,disk_availrate,current_time))
+        .format((ip_mac+"_"+ip_info),os_kind,os_ver,pc_name,ip_info,ip_mac,cpu_used,mem_ttl,mem_used,mem_userate,mem_availrate,down_ttl,up_ttl,down_speed,up_speed,disk_path,disk_ttl,disk_used,disk_userate,disk_availrate,current_time))
     
     #####DB 데이터 전송부분#####
     cursor.execute(sqlquery)    
@@ -86,9 +86,9 @@ def resource_trace():
 
 
 # 실행소스
-rep_time = 1                                           # 반복주기
-schedule.every(rep_time).minutes.do(resource_trace)    # 반복설정
-print("<데이터수집중({})... 수집주기:{}분>".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),rep_time))
+rep_time = int(input("수집주기를 입력해주세요(초 단위, 3600초=1시간) --> "))  # 반복주기
+schedule.every(rep_time).seconds.do(resource_trace)          # 반복설정
+print("<데이터수집중({})... 수집주기:{}초>".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),rep_time))
 
 # schedule.every(1).seconds.do(resource_trace)                 # 반복설정
 
